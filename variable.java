82a25986fe9a0b1d12fe4d6a49611468faae1cda
Subhajit Das <subhajit9004@gmail.com>
2025-09-26 23:18:47 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,20 @@
+public class variable {
+    public static void main(String[] args){
+        int yourNumber;
+        yourNumber = 45;
+        System.out.println(yourNumber);
+
+
+        float myFloat = 5.0f;
+        System.out.println(myFloat);
+
+        double myDouble = 1.089;
+        System.out.println(myDouble);
+
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian); 
+    }
+}
