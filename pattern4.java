7ce4a17c1b8ac9fc69956477daa9bcf41141bd4f
Subhajit Das <subhajit9004@gmail.com>
2025-09-26 23:18:26 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,10 @@
+public class pattern4 {
+     public static void main(String[] args){
+
+        System.out.println("*\n* *\n* * *\n* * * *");
+        System.out.println("* * * *\n* * *\n* *\n*");
+        System.out.println("      *\n    * *\n  * * *\n* * * *");
+     }
+
+}
+ 
