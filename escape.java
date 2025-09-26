256e9d86346d4e3666160b46f00ef00046a1e22e
Subhajit Das <subhajit9004@gmail.com>
2025-09-26 23:18:11 +0530

    Add files via upload

---
+++ escape.java
@@ -0,0 +1,12 @@
+public class escape {
+    public static void main(String[] args){
+
+
+        System.out.println("Hello\tworld");
+        System.out.println("Hello\bworld");
+        System.out.println("Hello\nworld");
+        System.out.println("Hello\'world\'");
+        System.out.println("Hello\"world\"");
+        System.out.println("Hello\\world\\");  
+    }
+}
