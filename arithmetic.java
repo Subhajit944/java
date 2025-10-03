d093df2ff5c4eafb12a1470af2dc90a1577942b9
Subhajit Das <subhajit9004@gmail.com>
2025-10-03 23:11:19 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
