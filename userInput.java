c9299e19d21844e6d50146871afcb12611bfb199
Subhajit Das <subhajit9004@gmail.com>
2025-10-03 23:08:35 +0530

    Add files via upload

---
+++ userInput.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your name :    ");
+        String name = input.nextLine();
+        System.out.println("Good Morning    " + name);
+        System.out.print(name + " , Also tell me your age");
+        int age = input.nextInt();
+        System.out.println("Your age is:    " + age);
+
+
+    }
+}
