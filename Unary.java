0113b39ade6ffae094766a204a1af9080b1a3c2e
Subhajit Das <subhajit9004@gmail.com>
2025-10-05 21:55:40 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,20 @@
+public class Unary {
+    public static void main(String[] args) {
+        
+        int x = 5;
+        int y = -x;
+        int z = -y;
+        System.out.println(z);
+
+        int a = 5;
+        ++a;
+        System.out.println(a);
+        -- a;
+        System.out.println(a);
+        a --;
+        System.out.println(a);
+        a ++;
+        System.out.println(a);
+
+    }
+}
