fb09ff049784ae0c5b4eefdbba10aeebd7d97d90
Subhajit Das <subhajit9004@gmail.com>
2025-10-24 23:05:17 +0530

    Add files via upload

---
+++ BitwiseOR.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class BitwiseOR {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Showcasing Bitwise  OR operator\n");
+        System.out.print("Please enter the  first number :   ");
+        int first = input.nextInt();
+        System.out.print("Now, enter the  second number:   ");
+        int second = input.nextInt();
+
+        int result = first | second;
+        System.out.println("Result is :   " + result);
+    }
+}
