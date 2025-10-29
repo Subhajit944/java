149d6b0326bf988cf716fd6583a7233419bf9213
Subhajit Das <subhajit9004@gmail.com>
2025-10-29 22:11:22 +0530

    Add files via upload

---
+++ OddEvenBiwise.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class OddEvenBiwise {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Odd/Even Calculator\n");
+        System.out.print("Please enter your number :    ");
+        int num = input.nextInt();
+
+        if((num & 1)== 1){
+            System.out.println("Your number is odd");
+
+        }else{
+            System.out.println("Your number is Even ");
+        }
+    }
+    
+}
+
+
+ 
