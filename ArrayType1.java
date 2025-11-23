e9ea226d67af7ca68b4010b34fbf03aceb3b839a
Subhajit Das <subhajit9004@gmail.com>
2025-11-23 21:48:01 +0530

    Add files via upload

---
+++ ArrayType1.java
@@ -0,0 +1,16 @@
+public class ArrayType1 {
+    public static void main(String[] args) {
+        int[] myArr = new int[5];
+        myArr[0] = 98;
+        myArr[1 ]= 65;
+        myArr[2 ]= 55;
+        myArr[3 ]= 45;
+        myArr[4 ]= 35;
+
+        System.out.println(myArr[0]);
+        System.out.println(myArr[1]);
+        System.out.println(myArr[2]);
+        System.out.println(myArr[3]);
+        System.out.println(myArr[4]);
+    }
+}
