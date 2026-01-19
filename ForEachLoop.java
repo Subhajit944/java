cc18fec8d54920d8d3366b4b32f04ea1fc70d75d
Subhajit Das <subhajit9004@gmail.com>
2026-01-19 21:52:22 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,15 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] array = new String[]{
+            "Ram", "Shyam","Mohan","Sohan","Sita","Geeta"
+        };
+
+        printArray(array);
+    }
+
+    public static void printArray(String[] array){
+        for(String name : array){
+            System.out.println(name);
+        }
+    }
+}
