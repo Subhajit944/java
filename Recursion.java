4e6ca12c3eca46fa481955b73029682ea6fd7c4c
Subhajit Das <subhajit9004@gmail.com>
2026-01-25 23:30:52 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Recursion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Factorial generator\n");
+        System.out.println("Please enter your number :  ");
+        int num = input.nextInt();
+        long fact =factorial(num);
+        System.out.println("Factorial of your number is :    " + fact);
+    }    
+
+    public static long factorial(int num){
+        System.out.println("Function called for :  " + num);
+        if(num == 1){
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+}
