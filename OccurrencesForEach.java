13b10ddca68fa1d9349e1bfc5cdbb7cf1338d4f6
Subhajit Das <subhajit9004@gmail.com>
2026-02-07 21:31:41 +0530

    Add files via upload

---
+++ OccurrencesForEach.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class OccurrencesForEach {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to finding occurrences");
+        int[] nums = inputArray();
+        System.out.println("Now , enter the number you want to search :   ");
+        int element = input.nextInt();
+        int occ = countOccurrences(nums, element);
+        System.out.println("Your element was found :  " + occ + " times");
+
+    }
+
+    public static int countOccurrences (int[] nums, int element){
+        int occ = 0;
+        for(int num : nums) {
+            if(num == element){
+                occ++;
+            }
+        }
+        return occ;
+    }
+
+
+    public static int[] inputArray(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the number of element :    ");
+        int size = input.nextInt();
+        int[] nums = new int[size];
+        int i = 0;
+
+        while( i < size){
+            System.out.print("please enter element no"+ (i+1) + " : ");
+            nums[i] = input.nextInt();
+            i++;
+        }
+        return nums;
+    }
+}
