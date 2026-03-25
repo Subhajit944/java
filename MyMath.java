1b1df24e8c0b37ff8468d16fda6e7253ab661d51
Subhajit Das <subhajit9004@gmail.com>
2026-03-25 08:13:28 +0530

    Add files via upload

---
+++ MyMath.java
@@ -0,0 +1,12 @@
+public class MyMath {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-99));
+        System.out.println(Math.ceil(5.07));
+        System.out.println(Math.floor(5.07));
+        System.out.println(Math.round(5.57));
+        System.out.println(Math.random());
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+    }
+}
