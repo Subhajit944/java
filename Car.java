5cb083c66d5f0c1986bfabe7bd7968c86d84c685
Subhajit Das <subhajit9004@gmail.com>
2026-04-02 07:49:23 +0530

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,27 +1,8 @@
-public class Car {
-    int noOfWheels;
-    int noOfDoors;
-    int maxSpeed;
-    String name;
-    String modleNumber;
-    String company;
+package polymorphism;
 
-    public Car( int noOfWheels, int noOfDoors, int maxSpeed, String name, String  modelNumber, String company){
-        this.noOfWheels = noOfWheels;
-        this.noOfDoors = noOfDoors;
-        this.maxSpeed =maxSpeed;
-        this.name = name;
-        this.modleNumber = modelNumber;
-        this.company = company;
+public class Car extends Vchicle {
+    
+    public int noOfDoors(){
+        return 5;
     }
-
-
-    public static void main(String[] args) {
-        
-        Car Swift = new Car(4, 4, 120, 
-             "Swift", "SW789", "tata");
-    }
-
-  
-
 }
