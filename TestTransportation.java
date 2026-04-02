3d1b4c8231a035641a790a3489ac57b39a29badb
Subhajit Das <subhajit9004@gmail.com>
2026-04-02 07:49:55 +0530

    Add files via upload

---
+++ TestTransportation.java
@@ -0,0 +1,17 @@
+package polymorphism;
+
+import Inheritance.Vehicle;
+
+
+public class TestTransportation {
+    public static void main(String[] args) {
+        
+        Car c = new Car();
+        Vehicle v = new Vehicle();
+
+        //Vehicle vCar = new Car();
+        //Car cVehicle =  (Car) new Vehicle();
+    }
+
+    
+}
