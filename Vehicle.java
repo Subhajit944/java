9ec166f0ef73f00fdd8e6ce7863f90b9fa4dcb5c
Subhajit Das <subhajit9004@gmail.com>
2026-04-04 19:14:58 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,10 @@
+package Inheritance;
+public class Vehicle {
+    
+    public int numberOfTires;
+
+    public void commute(){
+        System.out.printf("I am going from A to place B using %d tires", numberOfTires);
+
+    }
+}
