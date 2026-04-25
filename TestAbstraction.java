61058dd950e16d658a5671dfb35dec686270fdff
Subhajit Das <subhajit9004@gmail.com>
2026-04-25 23:03:00 +0530

    Add files via upload

---
+++ TestAbstraction.java
@@ -0,0 +1,10 @@
+package Abstraction;
+
+public class TestAbstraction {
+    public static void main(String[] args) {
+        //Vehicle veh = new Vehicle(2);
+        car Car = new car();
+        Car.commute();
+    }
+    
+}
