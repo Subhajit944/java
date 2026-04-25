584bdce277d0ca30f1aa46a6bf51e21caffa7877
Subhajit Das <subhajit9004@gmail.com>
2026-04-25 23:03:42 +0530

    Add files via upload

--- Vehicle.java
+++ Vehicle.java
@@ -1,10 +1,24 @@
-package Inheritance;
-public class Vehicle {
+package Abstraction;
+
+public  abstract class Vehicle {
+    private int noOfTires;
+
+    public abstract void makeStartSound();
+
     
-    public int numberOfTires;
 
+    public Vehicle(int noOfTires){
+        this.noOfTires = noOfTires;
+    }
+
+    public int getNoOfTires(){
+        return noOfTires;
+    }
+
+    public void setNoOfTires(int noOfTires){
+        this.noOfTires = noOfTires;
+    }
     public void commute(){
-        System.out.printf("I am going from A to place B using %d tires", numberOfTires);
-
+        System.out.println("going......");
     }
 }
