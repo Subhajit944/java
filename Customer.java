196daeb5f2a04b82f80223389174996cf0917e4c
Subhajit Das <subhajit9004@gmail.com>
2026-05-01 19:44:26 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,11 @@
+package challange78;
+
+public class Customer {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("001", "subhajit");
+        account.depositMoney(100);
+        System.out.println(account.withdrawMoney(200));
+        account.depositMoney(-40);
+        account.withdrawMoney(0);
+    }
+}
