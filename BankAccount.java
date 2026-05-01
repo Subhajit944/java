5b8bac043de096a888a60e41a7569b7cfd41b1d3
Subhajit Das <subhajit9004@gmail.com>
2026-05-01 19:44:03 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,36 @@
+package challange78;
+
+public class BankAccount {
+    
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName){
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+    }
+
+    public void depositMoney(double money){
+        if(money <= 0){
+            System.out.println("Invalid Deposit");
+
+        }else{
+             balance += money;
+        }
+    }
+
+    public double withdrawMoney(double money){
+        if(balance >= money){
+            balance -= money;
+        }else if(money <= 0){
+            System.out.println("Invalid Withdraw");
+        }else{
+            money = balance;
+            balance = 0;
+        }
+
+        return money;
+        
+    }
+}
