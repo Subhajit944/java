5dcbde9e6b5f577a33ec846b75d0c5641c6a18de
Subhajit Das <subhajit9004@gmail.com>
2026-08-30 23:21:37 +0530

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,9 +1,21 @@
-package geometry;
+package challenge83;
 
-public class Circle {
-    public double radius;
+public class Circle extends Shape{
+    
+    private double radiusInCms;
 
-    public Circle(double radius){
-        this.radius = radius;
+    public Circle(double radiusInCms){
+        this.radiusInCms = radiusInCms;
     }
+
+    public double getRadiusInCms(){
+        return radiusInCms;
+    }
+
+
+    @Override
+    public double calculateArea(){
+        return  Math.PI * Math.pow(radiusInCms, 2);
+    }
+
 }
